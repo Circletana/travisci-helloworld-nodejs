546de5a5e8e08f97843a13ea1f955a265bb465ce
Circletana <60434113+Circletana@users.noreply.github.com>
2020-02-01 02:47:59 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', async (req, res) => res.status(200).send('Hello World!'));
+
+module.exports = app;
+
+// By Montana Mendy (2020)
